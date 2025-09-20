44be1b62acc602621ee301be08a0531207456160
dominhduc03022004 <dominhduc03022004@gmail.com>
2025-09-20 18:26:18 +0700

    them dien tich

---
+++ lesson1/dist/hcn.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function chuvi(a, b) {
+    let result = (a + b) * 2;
+    return result;
+}
+function dientich(a, b) {
+    let result = (a * b);
+    return result;
+}
+console.log(chuvi(4, 5));
+console.log(dientich(3, 3));
---
+++ lesson1/hcn.ts
@@ -0,0 +1,12 @@
+function chuvi (a:number, b:number) {
+    let result:number = (a + b) * 2;
+    return result;
+}
+
+function dientich(a: number, b: number) {
+    let result:number = (a*b);
+    return result;
+}
+
+console.log(chuvi(4, 5));
+console.log(dientich(3,3));
