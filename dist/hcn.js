8e8528e5193dcc90871091ef93517037a8fe84be
Đỗ Minh Đức <dominhduc03022004@gmail.com>
2025-09-20 18:34:37 +0700

    Delete dist directory

--- dist/hcn.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function chuvi(a, b) {
-    let result = (a + b) * 2;
-    return result;
-}
-console.log(chuvi(4, 5));
--- dist/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello");
-let age = 20;
-// age = false;
-console.log(age);
